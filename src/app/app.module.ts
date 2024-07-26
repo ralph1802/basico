56f718f4709debfba16e8b3b62062a2cb8a42c49
Axel Leos <rleos1802@outlook.com>
2024-07-26 12:10:05 -0600

    creacion de carrusel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EtConsultaVetComponent } from './basic/shared/et-consulta-vet/et-consulta-vet.component';
+import { CarrucelServiciosComponent } from './basic/components/carrucel-servicios/carrucel-servicios.component';
 import { InicioComponent } from './basic/components/inicio/inicio.component';
 import { NavBarComponent } from './basic/components/nav-bar/nav-bar.component';
-import { EtFarmtaVetComponent } from './basic/shared/et-farmta-vet/et-farmta-vet.component';
+
+import { EtConsultaVetComponent } from './basic/shared/et-consulta-vet/et-consulta-vet.component';
+import { EtDomicilioVetComponent } from './basic/shared/et-domicilio-vet/et-domicilio-vet.component';
 import { EtEsteticaVetComponent } from './basic/shared/et-estetica-vet/et-estetica-vet.component';
+import { EtFarmtaVetComponent } from './basic/shared/et-farmta-vet/et-farmta-vet.component';
+import { EtLaboratorioVetComponent } from './basic/shared/et-laboratorio-vet/et-laboratorio-vet.component';
+import { EtPensionVetComponent } from './basic/shared/et-pension-vet/et-pension-vet.component';
+import { EtVentaVetComponent } from './basic/shared/et-venta-vet/et-venta-vet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    CarrucelServiciosComponent,
     InicioComponent,
     EtConsultaVetComponent,
-    EtFarmtaVetComponent,
+    EtDomicilioVetComponent,
     EtEsteticaVetComponent,
+    EtFarmtaVetComponent,
+    EtLaboratorioVetComponent,
+    EtPensionVetComponent,
+    EtVentaVetComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/basic/basic.module.ts
+++ src/app/basic/basic.module.ts
@@ -3,10 +3,14 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { EtConsultaVetComponent } from './shared/et-consulta-vet/et-consulta-vet.component';
 import { EtFarmtaVetComponent } from './shared/et-farmta-vet/et-farmta-vet.component';
 import { EtEsteticaVetComponent } from './shared/et-estetica-vet/et-estetica-vet.component';
+import { EtPensionVetComponent } from './shared/et-pension-vet/et-pension-vet.component';
+import { EtLaboratorioVetComponent } from './shared/et-laboratorio-vet/et-laboratorio-vet.component';
+import { EtVentaVetComponent } from './shared/et-venta-vet/et-venta-vet.component';
+import { EtDomicilioVetComponent } from './shared/et-domicilio-vet/et-domicilio-vet.component';
+import { CarrucelServiciosComponent } from './components/carrucel-servicios/carrucel-servicios.component';
 
 @NgModule({
   imports: [],
   exports: [],
-  declarations: [],
 })
 export class NameModule { }
